f884bf822bd1223748dfa0fcda68aaa6a2bd0f3e
Nigmatulin8 <rustam280897@gmail.com>
2020-01-14 17:54:58 +0400

    deploy

--- src/store/reducers.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import { userReducer } from './user/reducers.js';
-import { navReducer } from './nav/reducers.js';
-
-export default combineReducers({
-    user: userReducer,
-    nav: navReducer,
-});
